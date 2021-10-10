f317ed8179c9f6729e60913cfb856fb0893e9c5b
Jonathan Baloyi <Jonathan.Baloyi@dariel.co.za>
2021-10-10 13:19:15 +0200

    Fixed hamburger

--- src/app/components/core/navbar/navbar.component.ts
+++ src/app/components/core/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, HostListener, ElementRef } from '@angular/core';
 import { IMenu } from 'src/app/models/IMenu';
 
 @Component({
@@ -10,10 +10,29 @@ export class NavbarComponent implements OnInit {
 
   @Input()
   public menuItems: IMenu[];
+  public outsideClick = false;
+  public navbarOpen = false;
 
-  constructor() { }
+  constructor(private eRef: ElementRef) { }
 
   ngOnInit(): void {
+  }
+
+  ToggleNavBar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+
+  @HostListener('window:click', ['$event'])
+  clickout(event) {
+    if (this.eRef.nativeElement.contains(event.target)) {
+      if (this.navbarOpen) {
+        this.navbarOpen = false;
+      }
+
+      this.outsideClick = false;
+    } else {
+      this.outsideClick = true;
+    }
   }
 
 }
